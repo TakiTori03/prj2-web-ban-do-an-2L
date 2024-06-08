e1f6153750c060b715b2a794211d138e6cf637d1
Linh <tothailinhytbg2003@gmail.com>
2024-06-08 17:34:28 +0700

    add users.json

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI).then(con => {
-        console.log(`Mongodb Database connected with Host:${con.connection.host}`)
-    })
-}
-module.exports = connectDatabase
+  mongoose.connect(process.env.DB_LOCAL_URI).then((con) => {
+    console.log(
+      `Mongodb Database connected with Host:${con.connection.host}, port: ${con.connection.port}`
+    );
+  });
+};
+module.exports = connectDatabase;
