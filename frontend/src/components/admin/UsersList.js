e0a79dc6502c22ee205ee317a82b2ff7c5ea2bb8
Linh <tothailinhytbg2003@gmail.com>
2024-06-14 16:44:52 +0700

    add export excel user admin

--- frontend/src/components/admin/UsersList.js
+++ frontend/src/components/admin/UsersList.js
@@ -1,125 +1,155 @@
-import React, { Fragment, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { MDBDataTable } from 'mdbreact'
+import React, { Fragment, useCallback, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { MDBDataTable } from "mdbreact";
 
-import MetaData from '../layout/MetaData'
-import Loader from '../layout/Loader'
-import Sidebar from './Sidebar'
+import MetaData from "../layout/MetaData";
+import Loader from "../layout/Loader";
+import Sidebar from "./Sidebar";
 
-import { useAlert } from 'react-alert'
-import { useDispatch, useSelector } from 'react-redux'
-import { allUsers, deleteUser, clearErrors } from '../../actions/userActions'
-import { DELETE_USER_RESET } from '../../constants/userConstants'
+import { useAlert } from "react-alert";
+import { useDispatch, useSelector } from "react-redux";
+import { allUsers, deleteUser, clearErrors } from "../../actions/userActions";
+import { DELETE_USER_RESET } from "../../constants/userConstants";
+
+const xlsx = require("xlsx");
 
 const UsersList = ({ history }) => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
 
-    const alert = useAlert();
-    const dispatch = useDispatch();
+  const { loading, error, users } = useSelector((state) => state.allUsers);
+  const { isDeleted } = useSelector((state) => state.user);
 
-    const { loading, error, users } = useSelector(state => state.allUsers);
-    const { isDeleted } = useSelector(state => state.user)
+  useEffect(() => {
+    dispatch(allUsers());
 
-    useEffect(() => {
-        dispatch(allUsers());
-
-        if (error) {
-            alert.error(error);
-            dispatch(clearErrors())
-        }
-
-        if (isDeleted) {
-            alert.success('User deleted successfully');
-            history.push('/admin/users');
-            dispatch({ type: DELETE_USER_RESET })
-        }
-
-    }, [dispatch, alert, error, isDeleted, history])
-
-    const deleteUserHandler = (id) => {
-        dispatch(deleteUser(id))
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
     }
 
-    const setUsers = () => {
-        const data = {
-            columns: [
-                {
-                    label: 'User ID',
-                    field: 'id',
-                    sort: 'asc'
-                },
-                {
-                    label: 'Name',
-                    field: 'name',
-                    sort: 'asc'
-                },
-                {
-                    label: 'Email',
-                    field: 'email',
-                    sort: 'asc'
-                },
-                {
-                    label: 'Role',
-                    field: 'role',
-                    sort: 'asc'
-                },
-                {
-                    label: 'Actions',
-                    field: 'actions',
-                },
-            ],
-            rows: []
-        }
-
-        users.forEach(user => {
-            data.rows.push({
-                id: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-
-                actions: <Fragment>
-                    <Link to={`/admin/user/${user._id}`} className="btn btn-primary py-1 px-2">
-                        <i className="fa fa-pencil"></i>
-                    </Link>
-                    <button className="btn btn-danger py-1 px-2 ml-2" onClick={() => deleteUserHandler(user._id)}>
-                        <i className="fa fa-trash"></i>
-                    </button>
-                </Fragment>
-            })
-        })
-
-        return data;
+    if (isDeleted) {
+      alert.success("User deleted successfully");
+      history.push("/admin/users");
+      dispatch({ type: DELETE_USER_RESET });
     }
+  }, [dispatch, alert, error, isDeleted, history]);
 
+  const deleteUserHandler = (id) => {
+    dispatch(deleteUser(id));
+  };
 
-    return (
-        <Fragment>
-            <MetaData title={'All Users'} />
-            <div className="row">
-                <div className="col-12 col-md-2">
-                    <Sidebar />
-                </div>
+  const exportToExcel = useCallback(() => {
+    const processedData = users.map((item) => ({
+      ...item,
+      avatar_public_id: item.avatar.public_id,
+      avatar_url: item.avatar.url,
+      // Bỏ trường avatar
+      avatar: undefined,
+    }));
+    // Tạo một workbook mới
+    const workbook = xlsx.utils.book_new();
 
-                <div className="col-12 col-md-10">
-                    <Fragment>
-                        <h1 className="my-5">All Users</h1>
+    // Chuyển đổi dữ liệu thành định dạng sheet
+    const worksheet = xlsx.utils.json_to_sheet(processedData);
 
-                        {loading ? <Loader /> : (
-                            <MDBDataTable
-                                data={setUsers()}
-                                className="px-3"
-                                bordered
-                                striped
-                                hover
-                            />
-                        )}
+    // Thêm sheet vào workbook
+    xlsx.utils.book_append_sheet(workbook, worksheet, "Users");
 
-                    </Fragment>
-                </div>
-            </div>
+    // Xuất workbook ra file Excel
+    xlsx.writeFile(workbook, "users.xlsx");
+    alert.success("Lưu file thành công");
+  }, [users, alert]);
 
-        </Fragment>
-    )
-}
+  const setUsers = () => {
+    const data = {
+      columns: [
+        {
+          label: "User ID",
+          field: "id",
+          sort: "asc",
+        },
+        {
+          label: "Name",
+          field: "name",
+          sort: "asc",
+        },
+        {
+          label: "Email",
+          field: "email",
+          sort: "asc",
+        },
+        {
+          label: "Role",
+          field: "role",
+          sort: "asc",
+        },
+        {
+          label: "Actions",
+          field: "actions",
+        },
+      ],
+      rows: [],
+    };
 
-export default UsersList
+    users.forEach((user) => {
+      data.rows.push({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+
+        actions: (
+          <Fragment>
+            <Link
+              to={`/admin/user/${user._id}`}
+              className="btn btn-primary py-1 px-2"
+            >
+              <i className="fa fa-pencil"></i>
+            </Link>
+            <button
+              className="btn btn-danger py-1 px-2 ml-2"
+              onClick={() => deleteUserHandler(user._id)}
+            >
+              <i className="fa fa-trash"></i>
+            </button>
+          </Fragment>
+        ),
+      });
+    });
+
+    return data;
+  };
+
+  return (
+    <Fragment>
+      <MetaData title={"All Users"} />
+      <div className="row">
+        <div className="col-12 col-md-2">
+          <Sidebar />
+        </div>
+
+        <div className="col-12 col-md-10">
+          <Fragment>
+            <h1 className="my-5">All Users</h1>
+            <button onClick={exportToExcel}>Export Exel</button>
+
+            {loading ? (
+              <Loader />
+            ) : (
+              <MDBDataTable
+                data={setUsers()}
+                className="px-3"
+                bordered
+                striped
+                hover
+              />
+            )}
+          </Fragment>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default UsersList;
