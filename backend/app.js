53a9bc4420a3c71fe1938f9dc6aabbaa8112b6b6
TakiTori03 <146362556+TakiTori03@users.noreply.github.com>
2025-02-08 16:47:43 +0700

    commit before deploy

--- backend/app.js
+++ backend/app.js
@@ -1,48 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
-const cors = require('cors')
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 // const dotenv = require('dotenv');
-const path = require('path')
+const path = require("path");
 
-const errorMiddleware = require('./middlewares/errors')
+const errorMiddleware = require("./middlewares/errors");
 
-// Setting up config file 
-if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
+// Setting up config file
+if (process.env.NODE_ENV !== "PRODUCTION")
+  require("dotenv").config({ path: "backend/config/config.env" });
 // dotenv.config({ path: 'backend/config/config.env' })
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(fileUpload());
-app.use(cors())
+app.use(cors());
 
 // Import all routes
-const products = require('./routes/product');
-const auth = require('./routes/auth');
-const payment = require('./routes/payment');
-const order = require('./routes/order');
-const report = require('./routes/report');
+const products = require("./routes/product");
+const auth = require("./routes/auth");
+const payment = require("./routes/payment");
+const order = require("./routes/order");
+const report = require("./routes/report");
 
-app.use('/api/v1', products)
-app.use('/api/v1', auth)
-app.use('/api/v1', payment)
-app.use('/api/v1', order)
-app.use('/api/v1', report)
+app.use("/api/v1", products);
+app.use("/api/v1", auth);
+app.use("/api/v1", payment);
+app.use("/api/v1", order);
+app.use("/api/v1", report);
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+// if (process.env.NODE_ENV === 'PRODUCTION') {
+//     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-    })
-}
-
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+//     })
+// }
 
 // Middleware to handle errors
 app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
