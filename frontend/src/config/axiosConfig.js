bbe119ed24b89929cf38027b99be290694598119
TakiTori03 <146362556+TakiTori03@users.noreply.github.com>
2025-02-08 17:36:35 +0700

    all

--- frontend/src/config/axiosConfig.js
+++ frontend/src/config/axiosConfig.js
@@ -4,9 +4,6 @@ const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:4000";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default axiosInstance;
